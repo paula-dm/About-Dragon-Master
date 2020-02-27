57e423c40c5f5244973c16454fbe3eae439cee6f
Paulina Ziomkowska <paulina.ziomkowska@gmail.com>
2020-02-27 21:54:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+const heroSection = document.getElementById("banner");
+const heroTitle = document.querySelector(".hero__title");
+
+let imageCounter = 1;
+const changeHeroImage = function () {
+    imageCounter < 3 ? imageCounter++ : imageCounter = 1;
+    heroSection.classList = `banner banner--image${imageCounter}`;
+};
+
+const changeHeroImageToPrevious = function () {
+    imageCounter === 1 ? imageCounter = 3 : imageCounter--;
+    heroSection.classList = `banner banner--image${imageCounter}`;
+};
+
+let autoImageChanging;
+
+const startAutoImageChanging = function () {
+    clearInterval(autoImageChanging);
+    autoImageChanging = setInterval(changeHeroImage, 3000);
+};
+
+const stopAutoImageChanging = function () {
+    clearInterval(autoImageChanging);
+
+};
+
+heroSection.addEventListener('mouseenter', stopAutoImageChanging);
+heroSection.addEventListener('mouseleave', startAutoImageChanging);
+
+
+startAutoImageChanging();
